80c33b99e298ebd5eb4076831307d3932e46e5e8
junelyst <aau23@yandex.ru>
2018-09-21 23:54:20 +0300

    Задачи 2 урока

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -62,7 +62,13 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String {
+    return when {
+        (age%10 == 1)&&(age != 11)&&(age != 111) -> "$age год"
+        (age%10 in 2..4)&&(age !in 12..14) -> "$age года"
+        else -> "$age лет"
+    }
+}
 
 /**
  * Простая
@@ -73,7 +79,17 @@ fun ageDescription(age: Int): String = TODO()
  */
 fun timeForHalfWay(t1: Double, v1: Double,
                    t2: Double, v2: Double,
-                   t3: Double, v3: Double): Double = TODO()
+                   t3: Double, v3: Double): Double {
+    val s1 = t1*v1
+    val s2 = t2*v2
+    val s3 = t3*v3
+    var res = -1.0
+    val halfS = (s1 + s2 + s3)/2.0
+    if (halfS <= s1) res = halfS/v1
+        else if (s1+s2 < halfS) res = t1 + t2 + (halfS - s1 - s2)/v3
+            else res = t1 + (halfS-s1)/v2
+    return res
+}
 
 /**
  * Простая
@@ -86,7 +102,21 @@ fun timeForHalfWay(t1: Double, v1: Double,
  */
 fun whichRookThreatens(kingX: Int, kingY: Int,
                        rookX1: Int, rookY1: Int,
-                       rookX2: Int, rookY2: Int): Int = TODO()
+                       rookX2: Int, rookY2: Int): Int {
+    var res = -1
+    if ((kingX == rookX1) || (kingY == rookY1)) {
+        if ((kingX == rookX2) || (kingY == rookY2)) res = 3
+        else res = 1
+    }
+
+    if ((kingX == rookX2) || (kingY == rookY2)) {
+        if ((kingX == rookX1) || (kingY == rookY1)) res = 3
+        else res = 2
+    }
+
+    if ((kingX != rookX1) && (kingX != rookX2) && (kingY!= rookY1) && (kingY != rookY2)) res = 0
+    return res
+}
 
 /**
  * Простая
@@ -100,7 +130,16 @@ fun whichRookThreatens(kingX: Int, kingY: Int,
  */
 fun rookOrBishopThreatens(kingX: Int, kingY: Int,
                           rookX: Int, rookY: Int,
-                          bishopX: Int, bishopY: Int): Int = TODO()
+                          bishopX: Int, bishopY: Int): Int {
+    var res = -1
+    if ((kingX == rookX)||(kingY == rookY)) {
+        if (Math.abs(kingX-bishopX) == Math.abs(kingY-bishopY)) res = 3
+        else res = 1
+    }
+    else if (Math.abs(kingX-bishopX) == Math.abs(kingY-bishopY)) res = 2
+        else res = 0
+    return res
+}
 
 /**
  * Простая
@@ -110,7 +149,17 @@ fun rookOrBishopThreatens(kingX: Int, kingY: Int,
  * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
  * Если такой треугольник не существует, вернуть -1.
  */
-fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
+fun triangleKind(a: Double, b: Double, c: Double): Int {
+    val cosA = (b*b + c*c - a*a)/(2.0*b*c)
+    val cosB = (a*a + c*c - b*b)/(2.0*a*c)
+    val cosC = (a*a + b*b - c*c)/(2.0*a*b)
+    return when {
+        ((cosA > 0)&&(cosA < 1)) && ((cosB > 0)&&(cosB < 1)) && ((cosC > 0)&&(cosC < 1)) -> 0
+        (cosA == 0.0) || (cosB == 0.0) || (cosC == 0.0) -> 1
+        ((cosA > -1)&&(cosA < 0)) || ((cosB > -1)&&(cosB < 0)) || ((cosC > -1)&&(cosC < 0)) -> 2
+        else -> -1
+    }
+}
 
 /**
  * Средняя
@@ -120,4 +169,35 @@ fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
  * Найти длину пересечения отрезков AB и CD.
  * Если пересечения нет, вернуть -1.
  */
-fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = TODO()
+fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int {
+    var res = -2
+    if ((a == d) && (b == c)) res = Math.abs(a-b)
+    if (a < b) {
+        if (b < c){
+            if (b == c) res = 0
+            else res = -1
+        }
+        else if (b < d) {
+            if (c == b) res = 0
+            else res = b - c
+        }
+        else if (c == d)
+            res = 1
+            else res = d - c
+    }
+
+    if (c < a) {
+        if (d < a){
+            if (a == d) res = 0
+            else res = -1
+        }
+        else if (d < b) {
+            if (a == d) res = 0
+            else res = d - a
+        }
+        else if (a == b)
+            res = 1
+            else res = b - a
+    }
+    return res
+}
--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -17,7 +17,7 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean = (number/1000+number%1000/100)==(number%100/10+number%10)
 
 /**
  * Простая
@@ -26,7 +26,8 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
+        ((x1 == x2) || (y1 == y2) || (Math.abs((x1-x2)) == Math.abs((y1-y2))))
 
 
 /**
@@ -35,25 +36,37 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int {
+    val vis = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)
+    return when {
+        ((month == 4) || (month == 6) || (month == 9) || (month == 11)) -> 30
+        (month == 2) && (vis == true) -> 29
+        (month == 2) && (vis == false) -> 28
+        else -> 31
+    }
+}
 
-/**
- * Средняя
- *
- * Проверить, лежит ли окружность с центром в (x1, y1) и радиусом r1 целиком внутри
- * окружности с центром в (x2, y2) и радиусом r2.
- * Вернуть true, если утверждение верно
- */
-fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
 
-/**
- * Средняя
- *
- * Определить, пройдет ли кирпич со сторонами а, b, c сквозь прямоугольное отверстие в стене со сторонами r и s.
- * Стороны отверстия должны быть параллельны граням кирпича.
- * Считать, что совпадения длин сторон достаточно для прохождения кирпича, т.е., например,
- * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
- * Вернуть true, если кирпич пройдёт
- */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+
+    /**
+     * Средняя
+     *
+     * Проверить, лежит ли окружность с центром в (x1, y1) и радиусом r1 целиком внутри
+     * окружности с центром в (x2, y2) и радиусом r2.
+     * Вернуть true, если утверждение верно
+     */
+    fun circleInside(x1: Double, y1: Double, r1: Double,
+                     x2: Double, y2: Double, r2: Double): Boolean =
+            ((sqr(x1-x2) + sqr(y1-y2)) <= sqr(r1-r2)) && (r1 <= r2)
+
+    /**
+     * Средняя
+     *
+     * Определить, пройдет ли кирпич со сторонами а, b, c сквозь прямоугольное отверстие в стене со сторонами r и s.
+     * Стороны отверстия должны быть параллельны граням кирпича.
+     * Считать, что совпадения длин сторон достаточно для прохождения кирпича, т.е., например,
+     * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
+     * Вернуть true, если кирпич пройдёт
+     */
+    fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean =
+            ((a <= r) || (b <= r) || (c <= r)) && ((a <= s) || (b <= s) || (c <= s))
